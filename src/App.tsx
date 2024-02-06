fa32a22a0db0326a867bb1ee5857a84afc178fe0
Annalisa Comin <comin@cip.ifi.lmu.de>
2024-02-06 11:35:36 +0100

    added basic theme switcher functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import { ThemeContext } from "./ThemeProvider";
+import "./general.scss";
 
 function App() {
+  const { darkTheme, toggleTheme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      darkTheme ? "dark" : "light",
+    );
+  }, [darkTheme]);
+
   return (
-              <div className="grid">
-                  <h1>Social Media Dashboard</h1>
-                  <p>Total Followers: 23,004</p>
-                  <h2>Overview - Today</h2>
-              </div>
+    <div className="grid">
+      <h1>Social Media Dashboard</h1>
+      <p>Total Followers: 23,004</p>
+      <h2 onClick={toggleTheme}>Overview - Today</h2>
+      <button onClick={toggleTheme}>button</button>
+    </div>
   );
 }
 
