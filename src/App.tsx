3a3c61c5d9560ab5fa2310004c71609c514fb7f5
Annalisa Comin <comin@cip.ifi.lmu.de>
2024-02-08 11:38:18 +0100

    added top background

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ function App() {
   return (
     <div className="app">
       <div className="grid">
+        <div className="header-bg"></div>
         <header>
           <div>
             <h1>Social Media Dashboard</h1>
@@ -30,95 +31,93 @@ function App() {
             <Switch onSwitch={toggleTheme} sliderOnRightside={true} />
           </div>
         </header>
-        <main>
-          <div className="cards-grid">
-            <SocialMediaCard
-              followers={"1987"}
-              username={"nathanf"}
-              followerStats={12}
-              positive={true}
-              theme={"Facebook"}
-            />
-            <SocialMediaCard
-              followers={"1044"}
-              username={"nathanf"}
-              followerStats={99}
-              positive={true}
-              theme={"Twitter"}
-            />
-            <SocialMediaCard
-              followers={"11k"}
-              username={"realnathanf"}
-              followerStats={1099}
-              positive={true}
-              theme={"Instagram"}
-            />
-            <SocialMediaCard
-              followers={"8239"}
-              username={"Nathan F."}
-              followerStats={144}
-              positive={false}
-              theme={"YouTube"}
-            />
-            <h2>Overview - Today</h2>
-            <SocialMediaOverview
-              dataNumber={"87"}
-              theme={"Facebook"}
-              dataType={"Page Views"}
-              positive={true}
-              stats={3}
-            />
-            <SocialMediaOverview
-              dataNumber={"52"}
-              theme={"Facebook"}
-              dataType={"Likes"}
-              positive={false}
-              stats={2}
-            />
-            <SocialMediaOverview
-              dataNumber={"5462"}
-              theme={"Instagram"}
-              dataType={"Likes"}
-              positive={true}
-              stats={2257}
-            />
-            <SocialMediaOverview
-              dataNumber={"52k"}
-              theme={"Instagram"}
-              dataType={"Profile Views"}
-              positive={true}
-              stats={1375}
-            />
-            <SocialMediaOverview
-              dataNumber={"117"}
-              theme={"Twitter"}
-              dataType={"Retweets"}
-              positive={true}
-              stats={303}
-            />
+        <main className="cards-grid">
+          <SocialMediaCard
+            followers={"1987"}
+            username={"nathanf"}
+            followerStats={12}
+            positive={true}
+            theme={"Facebook"}
+          />
+          <SocialMediaCard
+            followers={"1044"}
+            username={"nathanf"}
+            followerStats={99}
+            positive={true}
+            theme={"Twitter"}
+          />
+          <SocialMediaCard
+            followers={"11k"}
+            username={"realnathanf"}
+            followerStats={1099}
+            positive={true}
+            theme={"Instagram"}
+          />
+          <SocialMediaCard
+            followers={"8239"}
+            username={"Nathan F."}
+            followerStats={144}
+            positive={false}
+            theme={"YouTube"}
+          />
+          <h2>Overview - Today</h2>
+          <SocialMediaOverview
+            dataNumber={"87"}
+            theme={"Facebook"}
+            dataType={"Page Views"}
+            positive={true}
+            stats={3}
+          />
+          <SocialMediaOverview
+            dataNumber={"52"}
+            theme={"Facebook"}
+            dataType={"Likes"}
+            positive={false}
+            stats={2}
+          />
+          <SocialMediaOverview
+            dataNumber={"5462"}
+            theme={"Instagram"}
+            dataType={"Likes"}
+            positive={true}
+            stats={2257}
+          />
+          <SocialMediaOverview
+            dataNumber={"52k"}
+            theme={"Instagram"}
+            dataType={"Profile Views"}
+            positive={true}
+            stats={1375}
+          />
+          <SocialMediaOverview
+            dataNumber={"117"}
+            theme={"Twitter"}
+            dataType={"Retweets"}
+            positive={true}
+            stats={303}
+          />
 
-            <SocialMediaOverview
-              dataNumber={"507"}
-              theme={"Twitter"}
-              dataType={"Likes"}
-              positive={true}
-              stats={553}
-            />
-            <SocialMediaOverview
-              dataNumber={"107"}
-              theme={"YouTube"}
-              dataType={"Likes"}
-              positive={false}
-              stats={19}
-            />
-            <SocialMediaOverview
-              dataNumber={"1407"}
-              theme={"YouTube"}
-              dataType={"Total Views"}
-              positive={false}
-              stats={12}
-            />
-          </div>
+          <SocialMediaOverview
+            dataNumber={"507"}
+            theme={"Twitter"}
+            dataType={"Likes"}
+            positive={true}
+            stats={553}
+          />
+          <SocialMediaOverview
+            dataNumber={"107"}
+            theme={"YouTube"}
+            dataType={"Likes"}
+            positive={false}
+            stats={19}
+          />
+          <SocialMediaOverview
+            dataNumber={"1407"}
+            theme={"YouTube"}
+            dataType={"Total Views"}
+            positive={false}
+            stats={12}
+          />
         </main>
       </div>
     </div>
